ecf3348216cc52a2a86b860faafb7dae29b7fde1
boring1hobby <iiriscoutinho@gmail.com>
2023-07-23 03:33:33 +0100

    1project

--- enviar_opiniao.php
+++ enviar_opiniao.php
@@ -1,8 +1,8 @@
 <?php
 // Inclua o arquivo PHPMailerAutoload.php do PHPMailer
-require 'C:\Users\Íris\Desktop\TESTES HTML CSS\css4\PHPMailer\src/PHPMailer.php';
-require 'C:\Users\Íris\Desktop\TESTES HTML CSS\css4\PHPMailer\src/SMTP.php';
-require 'C:\Users\Íris\Desktop\TESTES HTML CSS\css4\PHPMailer\src/Exception.php';
+require 'C:\Users\Íris\Desktop\TESTES HTML CSS\css4\PHPMailer-master\src/PHPMailer.php';
+require 'C:\Users\Íris\Desktop\TESTES HTML CSS\css4\PHPMailer-master\src/SMTP.php';
+require 'C:\Users\Íris\Desktop\TESTES HTML CSS\css4\PHPMailer-master\src/Exception.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
